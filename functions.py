9871201ee6d95255ef90627f067db50879692d09
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-18 01:47:02 -0500

    added functions

--- functions.py
+++ functions.py
@@ -1,24 +1,121 @@
 
-def maximum1(*args):
-    
-    list2= list(args)
-    if list2==[]:
-        return 0
-    maxi=list2[0]
-    list1=[]
+# def maximum1(*args):
    
-    for i in list2:
-        if i> maxi:
-            maxi= i
-            list1.append(i)
-    list3=list2.remove(maxi)
-    if list3==[]:
-        return
-    else:
-     maximum1(list3)
+#     if len(args)<=1:
+#         return
+#     else:
+#      list2=list(args)
+   
+#      maxi=list2[0]
+  
+    
+#      for i in list2:
+       
+       
+#         if i> maxi:
+#           list2.remove(maxi)
+#           maxi=i
+#           print(maxi)
+     
+#      maxi=list2[0]
+#      print(list2)
+#      print(maxi)
+     
+#      for i in list2:
            
-maximum1(2,9,4,0,1,4)
-
+       
+#         if i> maxi:
+#           maxi=i
+#           print(maxi)
+#      list2.remove(maxi)
+#      maxi=list2[0]
+#      print(list2)
+#      print(maxi)
+     
     
+     
+  #max function
 
+def max_num(a,b,c):
+  return max([a,b,c])
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+# multiplication
+
+def mult_list(lst):
+  #if empty list, return 0
+  if len(lst) == 0:
+    return 0
+  #product starts with first element of list
+  prod = lst[0]
+
+  #go through list elements and multiply them together
+  if len(lst) > 1:
+    for i in lst[1:]:
+      prod = prod * i
+
+  return prod
     
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
+
+# reverse
+
+def rev_string(my_str):
+  return my_str[::-1]
+
+print(rev_string(""))
+print(rev_string("apple"))
+print(rev_string("mt string"))
+
+# number range
+
+def num_within(x,a,b):
+  return x in range(a,b+1)
+     
+print(num_within(3,2,4))     
+print(num_within(3,1,3))     
+print(num_within(10,2,5))
+
+# Pascal triangle
+
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    for row in triangle:
+      print(row)
+
+pascal(2)
+pascal(3)
+        
+   
+   
+   
+    
+    
+# maximum1(2,9,4,0,3,8)
+
+
